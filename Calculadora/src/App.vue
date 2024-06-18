ba040fdc481080b1cdd5484b8f6dbb23be101154
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-06-18 15:42:57 -0300

    Inicio

---
+++ Calculadora/src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import TitleComponent from "./components/TitleComponent/title-component.vue"
+import { Calculator } from "./utils/calculator";
+
+export default {
+  name: "App",
+  data() {
+    let valueInput1 = 1
+  },
+  components: {
+    TitleComponent
+  },
+  computed: {
+    calculator(): Calculator {
+      return new Calculator()
+    }
+  },
+  methods:{
+    onclick() {
+      let result = this.calculator.division();
+    }
+  }
+
+}
+</script>
+
+<template>
+  <TitleComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ Calculadora/src/utils/calculator.ts
@@ -0,0 +1,17 @@
+export class Calculator {
+    sum (value1: number, value2: number):number {
+        return value1 + value2
+    }
+
+    subtraction (value1: number, value2: number): number {
+        return value1 - value2
+    }
+
+    multiply(value1: number, value2: number): number {
+        return value1 * value2
+    }
+    
+    division(value1: number, value2: number): number {
+        return value1 / value2
+    }
+}
